3d1e2deb96371ac5d91ac5139109aff84fe8bfc2
vivian Atieno <32508964+mevine@users.noreply.github.com>
2022-03-09 09:45:06 +0000

    Add files via upload

---
+++ palnd.py
@@ -0,0 +1,20 @@
+#check if a string is palendrom or not
+mev_str= 'rotor'
+rev_str=reversed(mev_str)
+
+#checking is string is same as its reverse
+if list(mev_str)==list(rev_str):
+    print("the statement is palendrom")
+else:
+        print("statement not palendrom")
+
+#statement not palendrom
+mev_str= 'mevine'
+rev_str=reversed(mev_str)
+
+#checking is string is same as its reverse
+if list(mev_str)==list(rev_str):
+    print("the statement is palendrom")
+else:
+        print("statement not palendrom")
+
